578db843a9618e8b4bc89c0b15eadfeb40e7352a
William Casarin <bill@casarin.me>
2015-09-12 16:08:45 -0700

    Add docs

--- src/Data/Bson/Lens.hs
+++ src/Data/Bson/Lens.hs
@@ -27,8 +27,10 @@ module Data.Bson.Lens
 
 , key
 , values
-, docValues
 , fields
+, members
+, nth
+, labels
 
 , AsValue(..)
 , AsDocument(..)
@@ -44,6 +46,14 @@ $(makePrisms ''Value)
 
 
 class AsDocument t where
+  -- |
+  -- A traversal for types that contain documents
+  --
+  -- >>> ["key" .= Bool True] ^? _Document
+  -- Just [ key: True]
+  --
+  -- >>> Doc ["key" .= Bool True] ^? _Document
+  -- Just [ key: True]
   _Document :: Traversal' t Document
 
 instance AsDocument Value where
@@ -56,6 +66,14 @@ instance AsDocument Javascript where
   _Document = _JavascriptDoc
 
 class AsValue t where
+  -- |
+  -- A traversal for types that contain 'Value's
+  --
+  -- >>> ("key" .= Bool True) ^? _Value
+  -- Just True
+  --
+  -- >>> Doc ["key" .= Bool True] ^? _Value
+  -- Just [ key: True]
   _Value :: Traversal' t Value
 
 instance AsValue Value where
@@ -65,45 +83,105 @@ instance AsValue Field where
   _Value = lens (\(_ := v)   -> v)
                 (\(k := _) v -> k := v)
 
-_Label :: Lens' Field Label
-_Label = lens (\(k := _)   -> k)
-              (\(_ := v) k -> k := v)
-
 _JavascriptDoc :: Lens' Javascript Document
 _JavascriptDoc = lens (\(Javascript d _) -> d)
                       (\(Javascript _ v) d -> Javascript d v)
 
+-- |
+-- Traversal into an 'Val' over a 'Value'
+--
+-- >>> Array [String "hey"] ^? nth 0 . _Val :: Maybe Text
+-- Just "hey"
+--
+-- >>> "[10.5]" ^? nth 0 . _Integer
+-- Just 10
+--
+-- >>> "42" ^? _Integer
+-- Just 42
 _Val :: (AsValue t, Val v) => Traversal' t v
 _Val = _Value.prism' val cast'
 
 _IndexedDocument :: Iso' Document IndexedDocument
 _IndexedDocument = iso IndexedDocument runIndexedDocument
 
-_IndexedDoc :: AsValue v => Traversal' v IndexedDocument
-_IndexedDoc = _Value . _Doc . _IndexedDocument
-
 _Symbol :: AsValue v => Traversal' v Text
-_Symbol = _Value . _Sym . iso (\(Symbol t) -> t) Symbol
+_Symbol = _Value._Sym.iso (\(Symbol t) -> t) Symbol
 
-field :: AsDocument t => Label -> Traversal' t Field
-field k = _Document . _IndexedDocument . ix k
 
+-- |
+-- A lens for a Label in a Field 
+--
+-- >>> ("someKey" := Float 3.0) ^. _Label
+-- "someKey"
+--
+-- >>> ("someKey" := Float 3.0) & _Label %~ Data.Text.toUpper
+-- SOMEKEY: 3.0
+_Label :: Lens' Field Label
+_Label = lens (\(k := _)   -> k)
+              (\(_ := v) k -> k := v)
+
+-- |
+-- Like 'ix', but for 'Document' values with Text indices
+--
+-- >>> ["someKey" := Float 3.0] ^? key "someKey"
+-- Just 3.0
 key :: AsDocument t => Label -> Traversal' t Value
-key k = field k . _Value
+key k = field k._Value
 
-docValues :: AsDocument v => Traversal' v Value
-docValues = fields . _Value
 
-fields :: AsDocument v => Traversal' v Field
-fields = _Document . traversed
+-- |
+-- A traversal for a specific Document 'Field'.
+-- This is similar to 'key' except it can target labels as well as values
+--
+-- >>> ["someKey" := Float 3.0] & field "someKey"._Label .~ "someOtherKey"
+-- [ someOtherKey: 3.0 ]
+field :: AsDocument t => Label -> Traversal' t Field
+field k = _Document._IndexedDocument.ix k
 
+
+-- | An indexed Traversal into Document fields
+--
+-- >>> Array [Doc ["doc1" := Float 3.0], Doc ["doc2" := Bool True]] ^.. values . fields
+-- [ key: 3.0, key2: True ]
+fields :: AsDocument v => IndexedTraversal' Int v Field
+fields = _Document.traversed
+
+-- | A Traversal into Document keys
+--
+-- >>> ["key" := Float 3.0, "key2" := Bool True] & labels %~ Data.Text.toUpper
+-- ["KEY": 3.0, "KEY2": True]
+labels :: AsDocument v => Traversal' v Label
+labels = fields._Label
+
+-- | A Traversal into Document values
+--
+-- >>> ["key" := Float 3.0, "key2" := Bool True] & members . _Float *~ 2
+-- [ key: 6.0, key2: True ]
+members :: AsDocument v => Traversal' v Value
+members = fields._Value
+
+
+-- | An indexed Traversal into Array elements
+--
+-- >>> Array [String "hey", Float 2.0] ^.. values
+-- ["hey", 2.0]
+--
+-- >>> "Array [Float 2.0,Float 3.0,Float 4.0]" & values . _Float *~ 2.0
+-- "[4.0,6.0,8.0]"
 values :: AsValue v => IndexedTraversal' Int v Value
-values = _Value . _Array . traversed
+values = _Value._Array.traversed
 
-type instance IxValue Value = Value
-type instance Index Value = Text
-instance Ixed Value where
-  ix i = _IndexedDoc . ix i . _Value
+
+-- | Like 'ix', but for Arrays with Int indexes
+--
+-- >>> "Array [Float 1.0, Float 2.0, Float 3.0]" ^? nth 1
+-- Just 2.0
+--
+-- >>> "Array [Float 1.0, Float 2.0, Float 3.0]" & nth 1 .~ Float 20.0
+-- "[1.0,20.0,3.0]"
+nth :: AsValue v => Int -> Traversal' v Value
+nth i = _Value._Array.ix i
+
 
 newtype IndexedDocument = IndexedDocument { runIndexedDocument :: Document }
 type instance IxValue IndexedDocument = Field
