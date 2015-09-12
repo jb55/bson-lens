921b01a5cd762a818cdaea8d2c29587f1866673b
William Casarin <bill@casarin.me>
2015-09-11 22:13:58 -0700

    Bunch of updates
      * AsDocument
      * AsValue
      * bunch of new document traversals

--- src/Data/Bson/Lens.hs
+++ src/Data/Bson/Lens.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Data.Bson.Lens
 ( _Float
@@ -25,6 +26,12 @@ module Data.Bson.Lens
 , _Int64
 
 , key
+, values
+, docValues
+, fields
+
+, AsValue(..)
+, AsDocument(..)
 )
 where
 
@@ -35,27 +42,76 @@ import Data.Text
 
 $(makePrisms ''Value)
 
-newtype IndexedDocument = IndexedDocument { runIndexedDocument :: Document }
 
--- instance Wrapped IndexedDocument where
---   type Unwrapped IndexedDocument = Document
---   _Wrapped' = iso runIndexedDocument IndexedDocument
+class AsDocument t where
+  _Document :: Traversal' t Document
 
-type instance IxValue IndexedDocument = Value
-type instance Index IndexedDocument = Text
-instance Ixed IndexedDocument where
-  ix k f xs0 = IndexedDocument <$> go (runIndexedDocument xs0) k
-    where
-      go [] _     = pure []
-      go (kv@(k' := v):as) k
-        | k == k'   = ((:=) <$> pure k' <*> f v) <&> (:as)
-        | otherwise = (kv:) <$> go as k
+instance AsDocument Value where
+  _Document = _Doc
+
+instance AsDocument Document where
+  _Document = id
+
+instance AsDocument Javascript where
+  _Document = _JavascriptDoc
+
+class AsValue t where
+  _Value :: Traversal' t Value
+
+instance AsValue Value where
+  _Value = id
+
+instance AsValue Field where
+  _Value = lens (\(_ := v)   -> v)
+                (\(k := _) v -> k := v)
+
+_Label :: Lens' Field Label
+_Label = lens (\(k := _)   -> k)
+              (\(_ := v) k -> k := v)
+
+_JavascriptDoc :: Lens' Javascript Document
+_JavascriptDoc = lens (\(Javascript d _) -> d)
+                      (\(Javascript _ v) d -> Javascript d v)
+
+_Val :: (AsValue t, Val v) => Traversal' t v
+_Val = _Value.prism' val cast'
 
 _IndexedDocument :: Iso' Document IndexedDocument
 _IndexedDocument = iso IndexedDocument runIndexedDocument
 
-_IndexedDoc :: Prism' Value IndexedDocument
-_IndexedDoc = _Doc . _IndexedDocument
+_IndexedDoc :: AsValue v => Traversal' v IndexedDocument
+_IndexedDoc = _Value . _Doc . _IndexedDocument
 
-key :: Label -> Traversal' Value Value
-key field = _IndexedDoc . ix field
+_Symbol :: AsValue v => Traversal' v Text
+_Symbol = _Value . _Sym . iso (\(Symbol t) -> t) Symbol
+
+field :: AsDocument t => Label -> Traversal' t Field
+field k = _Document . _IndexedDocument . ix k
+
+key :: AsDocument t => Label -> Traversal' t Value
+key k = field k . _Value
+
+docValues :: AsDocument v => Traversal' v Value
+docValues = fields . _Value
+
+fields :: AsDocument v => Traversal' v Field
+fields = _Document . traversed
+
+values :: AsValue v => IndexedTraversal' Int v Value
+values = _Value . _Array . traversed
+
+type instance IxValue Value = Value
+type instance Index Value = Text
+instance Ixed Value where
+  ix i = _IndexedDoc . ix i . _Value
+
+newtype IndexedDocument = IndexedDocument { runIndexedDocument :: Document }
+type instance IxValue IndexedDocument = Field
+type instance Index IndexedDocument = Text
+instance Ixed IndexedDocument where
+  ix i f xs0 = IndexedDocument <$> go (runIndexedDocument xs0) i
+    where
+      go [] _     = pure []
+      go (kv@(k' := v):as) k
+        | i == k'   = f (k := v) <&> (:as)
+        | otherwise = (kv:) <$> go as i
