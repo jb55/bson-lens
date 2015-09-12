01c1a91bc49d89de14d302b0bc63b09b05c281e9
William Casarin <bill@casarin.me>
2015-09-11 20:43:35 -0700

    Add text

--- bson-lens.nix
+++ bson-lens.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, profunctors, bson, lens, stdenv }:
+{ mkDerivation, base, profunctors, bson, lens, text, stdenv }:
 mkDerivation {
   pname = "bson-lens";
   version = "0.1.0";
   src = ./.;
-  libraryHaskellDepends = [ base profunctors bson lens ];
+  libraryHaskellDepends = [ base profunctors bson lens text ];
   homepage = "https://github.com/jb55/bson-lens";
   description = "BSON lenses";
   license = stdenv.lib.licenses.mit;
